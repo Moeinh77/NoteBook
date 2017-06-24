3e79f5dd33ed413857f031ee97f765be3a8d105e
Moeinh77 <moeinhasani77@gmail.com>
2017-06-24 16:11:10 +0430

    Initial commit

---
+++ app/src/main/java/com/hasani/moein/taan/tinynotebook/Notesdetails_forlist.java
@@ -0,0 +1,13 @@
+package com.hasani.moein.taan.tinynotebook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Notesdetails_forlist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notesdetails_forlist);
+    }
+}
