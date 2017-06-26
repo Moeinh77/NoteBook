36a58b61a3104ae8e5bab6250ba029f9d6625346
Moeinh77 <moeinhasani77@gmail.com>
2017-06-26 19:34:38 +0430

    step 1 complete

---
+++ app/src/main/java/Data/Constants.java
@@ -0,0 +1,16 @@
+package Data;
+
+/**
+ * Created by Moein on 6/26/2017.
+ */
+
+public class Constants {
+    public static final String DATABASE_NAME="notebookdb";
+    public static final int DATABASE_VERSION=1;
+    public static final String TABLE_NAME="notes";
+    public static final String TITLE_NAME="title";
+    public static final String CONTENT_NAME="content";
+    public static final String DATE_NAME="date";
+    public static final String KEY_ID="_id";
+
+}
---
+++ app/src/main/java/Data/DataBaseHandler.java
@@ -0,0 +1,82 @@
+package Data;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.zip.CheckedOutputStream;
+
+import Model.note;
+
+/**
+ * Created by Moein on 6/26/2017.
+ */
+
+public class DataBaseHandler extends SQLiteOpenHelper {
+
+    private final ArrayList<note> noteArrayList=new ArrayList<>();
+
+    public DataBaseHandler(Context context) {
+        super(context, Constants.DATABASE_NAME, null, Constants.DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String TABLE_CREATION=
+                "create table"+ Constants.TABLE_NAME+"("+Constants.KEY_ID+"INTEGER PRIMARY KEY"+
+                Constants.TITLE_NAME+"TEXT"+Constants.CONTENT_NAME+"TEXT"+Constants.DATE_NAME+"LONG);";
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL("drop table if exists "+Constants.TABLE_NAME);
+        onCreate(db);
+
+    }
+
+    public void addnote(note new_note){
+
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues values=new ContentValues();
+
+        values.put(Constants.TITLE_NAME,new_note.getTitle());
+        values.put(Constants.CONTENT_NAME,new_note.getContent());
+        values.put(Constants.DATE_NAME,java.lang.System.currentTimeMillis());
+        db.insert(Constants.TABLE_NAME,null,values);
+        db.close();
+
+    }
+
+    public ArrayList<note> getnotes(){
+
+        String select="select * from "+Constants.TABLE_NAME;
+        SQLiteDatabase db=this.getReadableDatabase();
+
+        Cursor cursor=db.query(Constants.TABLE_NAME,new String[]{Constants.KEY_ID,
+                Constants.TITLE_NAME,Constants.CONTENT_NAME,Constants.DATE_NAME}
+                ,null,null,null,null,Constants.DATABASE_NAME+"DESC");
+
+        if(cursor.moveToFirst()){
+            do{
+                note mynote=new note();
+                mynote.setTitle(cursor.getString(cursor.getColumnIndex(Constants.TITLE_NAME)));
+                mynote.setContent(cursor.getString(cursor.getColumnIndex(Constants.CONTENT_NAME)));
+                java.text.DateFormat dateFormat=java.text.DateFormat.getDateInstance();//new ***
+                String date_=dateFormat.format(new Date(cursor.getString(cursor.getColumnIndex(Constants.DATE_NAME))).getTime());
+                //Date depricatted
+                mynote.setDate(date_);
+                noteArrayList.add(mynote);
+
+            }while (cursor.moveToNext());
+        }
+        return noteArrayList;
+    }
+}
